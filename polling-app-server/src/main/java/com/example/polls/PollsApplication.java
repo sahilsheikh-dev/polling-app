4dd90cc54c61f3b022ee395f8a8e13893d8b674c
Sheikh, Sahil (Sahil) **CTR** <sheikh9@avaya.com>
2023-08-29 23:47:41 +0530

    machineIPBugFixed-29082023:2347-sahilsheikh.dev@gmail.com

--- polling-app-server/src/main/java/com/example/polls/PollsApplication.java
+++ polling-app-server/src/main/java/com/example/polls/PollsApplication.java
@@ -3,6 +3,7 @@ package com.example.polls;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,18 +15,21 @@ import com.example.polls.repository.UserRepository;
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class PollsApplication {
 
+	@Value("${pollsapp.machineIp}")
+    private String myAppMessage;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PollsApplication.class, args);
 	}
+
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				// registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
-				// registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE");
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+				System.out.println("STATUS_IP" + myAppMessage);
+				registry.addMapping("/**").allowedOrigins("http://" + myAppMessage + ":8081").allowedMethods("*");
 			}
 		};
 	}
